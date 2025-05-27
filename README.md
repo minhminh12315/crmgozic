9d740495ce775d6c4813d7bef111a6a9367a2905
Quang Minh <157796411+minhminh12315@users.noreply.github.com>
2025-05-27 13:06:39 +0700

    sua route, cách init trong readMe

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
-init
+Các bước init
 
-Tạo DB "crmgozic"
+ - Tạo DB "crmgozic"
 
-cd backend
+cmd: 
+python -m venv venv
 .\venv\Scripts\activate
+cd backend
 pip install -r requirements.txt
+python manage.py migrate
 python manage.py runserver 8000
 
-ctrl+shift+5
+tạo thêm 1 terminal nữa để chạy angular
 cd frontend
 npm install
 npm start
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'sign-in', component: SignInComponent },
+
+];
