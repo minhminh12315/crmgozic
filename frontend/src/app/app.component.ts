ea07067c733bbe895192aab358b30e549e06c11d
Lê Đức Ân <162079456+anduc0104@users.noreply.github.com>
2025-05-28 12:05:34 +0700

    An: sidenbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
-    HeaderComponent
+    HeaderComponent, SideBarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
